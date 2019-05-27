3671b89e7e18f05920a05d485bdf06fa1e3e0cc9
goni <gonina@gmail.com>
2019-05-27 13:39:35 +0300

    updated

--- groupby_data.py
+++ groupby_data.py
@@ -11,46 +11,61 @@ def abs_value(value):
     value = abs(value)
     return value
 
-def mat_to_df(fname):
-    mat = sio.loadmat(fname)
-    # time_acc = mat['timeAcc'][1000:100000] # specific data
-    # new_z = mat['new_z'][1000:100000] # specific data
-    time_acc = mat['timeAcc'] # all data
-    new_z = mat['new_z'] # all data
-    df_time = pd.DataFrame(data = time_acc, columns=['Time'])
-    df_acc = pd.DataFrame(data = new_z, columns=['Acc'])
-    df_all = pd.concat([df_time, df_acc], axis=1, sort = True)
-    df = df_all.copy()
-    df['real time'] = df['Time'].apply(mat_to_py_time)
-    df['Acc'] = df['Acc'].apply(abs_value)
-    df.pop('Time')
-    return df
-    # print (df2)
+class GroupData:
 
-def groupby_df(df):
+    def __init__(self, fname, output1, output2):
+        self.fname = fname
+        self.output1 = output1
+        self.output2 = output2
+        self.df = None
 
-    df['real time'] = pd.to_datetime(df['real time'])
-    hour = pd.to_timedelta(df['real time'].dt.hour, unit='H')
-    day = pd.to_timedelta(df['real time'].dt.day, unit='d')
+    def mat_to_df(self):
+        mat = sio.loadmat(self.fname)
+        time_acc = mat['timeAcc'][0:2000] # specific data
+        new_z = mat['new_z'][0:2000] # specific data
+        # time_acc = mat['timeAcc'] # all data
+        # new_z = mat['new_z'] # all data
+        df_time = pd.DataFrame(data = time_acc, columns=['Time'])
+        df_acc = pd.DataFrame(data = new_z, columns=['Acc'])
+        df_all = pd.concat([df_time, df_acc], axis=1, sort = True)
+        df = df_all.copy()
+        df['real time'] = df['Time'].apply(mat_to_py_time)
+        df['Acc'] = df['Acc'].apply(abs_value)
+        df.pop('Time')
+        self.df = df
+        # return df
+        # print (df2)
 
-    # mean_h = df.groupby(hour).mean()
-    # mean_d = df.groupby(day).mean()
-    # day_hour_mean = df.groupby([day, hour]).mean()
-    # day_hour_mean = df.groupby([day, hour]).mean()
-    # day_hour_min = df.groupby([day, hour]).min()
-    # day_hour_max = df.groupby([day, hour]).max()
-    # day_hour_max = df.groupby([day, hour]).std()
-    # day_hour_count = df.groupby([day, hour]).count()
+    def groupby_df(self):
 
-    grouped = df.groupby([day, hour]).aggregate(['min', max, np.median, np.std, np.mean])
-    grouped.to_csv("grouped_data.csv")
-    
+        self.df['real time'] = pd.to_datetime(self.df['real time'])
+        hour = pd.to_timedelta(self.df['real time'].dt.hour, unit='H')
+        day = pd.to_timedelta(self.df['real time'].dt.day, unit='d')
 
-    # grouped = pd.DataFrame(columns = ['count'],['mean'],['max'],['min'],['std'])
+        # mean_h = df.groupby(hour).mean()
+        # mean_d = df.groupby(day).mean()
+        # day_hour_mean = df.groupby([day, hour]).mean()
+        # day_hour_mean = df.groupby([day, hour]).mean()
+        # day_hour_min = df.groupby([day, hour]).min()
+        # day_hour_max = df.groupby([day, hour]).max()
+        # day_hour_max = df.groupby([day, hour]).std()
+        # day_hour_count = df.groupby([day, hour]).count()
 
-    # print (day_hour_mean, day_hour_min, day_hour_max)
-    # print (grouped)
-    return grouped
+        grouped_day = self.df.groupby([day]).aggregate(['min', max, np.median, np.std, np.mean])
+        grouped_day.to_csv(self.output1)
+        grouped_day_hour = self.df.groupby([day, hour]).aggregate(['min', max, np.median, np.std, np.mean])
+        grouped_day_hour.to_csv(self.output2)
+        
+
+        # grouped = pd.DataFrame(columns = ['count'],['mean'],['max'],['min'],['std'])
+
+        # print (day_hour_mean, day_hour_min, day_hour_max)
+        # print (grouped)
+        # return grouped_day, grouped_day_hour
+
+    def run (self):
+        self.mat_to_df()
+        self.groupby_df()
 
 
 
@@ -58,12 +73,13 @@ def groupby_df(df):
 
 if __name__ == "__main__":
 
-    fname = 'SmoothedFile.mat'
-    data = mat_to_df(fname)
-    # print (data)
-    data_grouped = groupby_df(data)
+    fname = 'SmoothedFile.mat' # write here file name
+    output1 = "grouped_day.csv" # change output file name here
+    output2 = "grouped_data_day_hour.csv" # change output file name here
 
-    # pd.DataFrame(columns=['two', 'three'])
+    yeals_data = GroupData (fname, output1, output2)
+    yeals_data.run()
+   
 
   
   
